1cd229e7a97a745cbef79fa56f6d1dfc48d0bf04
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-02-02 16:02:49 +0100

    push

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ const items = [
     }
 ];
 
-
+console.log(items);
 
 const itemsRef = document.getElementsByClassName('items')[0];
 const thumbsRef = document.getElementsByClassName('thumbs')[0];
@@ -60,7 +60,27 @@ let item = '';
 let thumb = '';
 let active = 1;
 
+for (let i = 0; i < items.length; i++) {
+    // item += `
+    //         <div class="item">
+    //             <img src="${items[i]}" alt="">
+    //             <div class="text">
+    //                 <h3>${title[i]}</h3>
+    //                 <p>${text[i]}</p>
+    //             </div>
+    //         </div>`
+    // thumb += `
+    //         <div class="thumb">
+    //             <img src="${items[i]}" alt="">
+    //         </div>
+    ` 
+}
+const classi = [
+    ['Classe 1', 10],
+    ['Classe 2', 15]
+];
 
+console.log(classi[1][1]);
 // for (let i = 0; i < items.length; i++) {
 //     item += `
 //         <div class="item">
